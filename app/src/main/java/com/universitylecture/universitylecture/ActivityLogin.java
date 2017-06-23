068bd8bdb7fa1f2b649694bc7a7e21a1f14f1179
Ruan <609387481@qq.com>
2017-06-23 20:45:41 +0800

    updata the login and register interface

---
+++ app/src/main/java/com/universitylecture/universitylecture/ActivityLogin.java
@@ -0,0 +1,32 @@
+package com.universitylecture.universitylecture;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static android.view.Window.FEATURE_NO_TITLE;
+
+/**
+ * Created by fengqingyundan on 2017/6/20.
+ */
+
+public class ActivityLogin extends AppCompatActivity {
+    private Button registerButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+        registerButton = (Button) findViewById(R.id.button6);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityLogin.this , ActivityRegisterFirst.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/universitylecture/universitylecture/LectureCircleFragment.java
+++ app/src/main/java/com/universitylecture/universitylecture/LectureCircleFragment.java
@@ -1,10 +1,12 @@
 package com.universitylecture.universitylecture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 //讲座圈界面
 public class LectureCircleFragment extends Fragment {
@@ -12,8 +14,18 @@ public class LectureCircleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lecture_circle, container, false);
+
+        Button test_login = (Button) view.findViewById(R.id.test_login);
+        test_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lecture_circle, container, false);
+        return view;
     }
 
 }
